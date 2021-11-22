6f208b4647eda819d3b1eb0dcc881d5febff0e51
Endy Suryo <endysuryalaksana@gmail.com>
2021-11-22 14:06:57 +0700

    remove user modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,12 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TodosModule } from './todos/todos.module';
-import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     GraphQLModule.forRoot({
       autoSchemaFile: true,
     }),
-    UsersModule,
     TodosModule,
   ],
   controllers: [],
